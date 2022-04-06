78dd4199339d1a9c02c8e765eb31bf5224a23ee0
Galahad <77021667+Galahaaad@users.noreply.github.com>
2022-04-06 17:25:01 +0200

    initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,23 @@
 const buttonVideo = document.querySelector('.seeFeetures');
 const presentationVideo = document.querySelector('.airpodsVideo');
 const closeVideo = document.querySelector('.close');
+const allPage = document.body;
+
+
 
 buttonVideo.addEventListener('click', function() {
     presentationVideo.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    presentationVideo.muted = false;
 });
 
 closeVideo.addEventListener('click', function() {
     presentationVideo.style.display = 'none';
+    document.body.style.overflow = 'auto';
+    presentationVideo.muted = true;
+
 });
+
+
 
 
